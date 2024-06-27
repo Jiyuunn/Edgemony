a08d53ae4a834655ea25d7c810f2c3aafc914c14
Jiyuun <167774681+Jiyuunn@users.noreply.github.com>
2024-06-27 17:26:57 +0200

    Add files via upload

---
+++ Edgemony/Esercitazioni/27-06-2024/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'; 
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Calculator</h1>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
