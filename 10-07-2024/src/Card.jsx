a24d96d1f303d483af3e67af4ddd74e98c16b82e
Jiyuun <167774681+Jiyuunn@users.noreply.github.com>
2024-07-10 20:29:41 +0200

    10-07-2024 added

--- 10-07-2024/src/Card.jsx
+++ 10-07-2024/src/Card.jsx
@@ -5,12 +5,12 @@ import './Card.css';
 const Card = ({ image1, image2, title1, title2, children }) => {
     return (
         <div className="card">
-            <div className="card-title">{title1}</div>
             <img className="card-image" src={image1} alt="Card" />
             <div className="card-hover">
                 <img className="card-image-hover" src={image2} alt="Card" />
                 <div className="card-title-hover">{title2}</div>
             </div>
+            <div className="card-title">{title1}</div>
             {children}
         </div>
     );
