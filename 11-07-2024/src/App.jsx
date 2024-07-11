f1afb85f2354eeeb808d5db8416b648737ae6feb
Jiyuun <167774681+Jiyuunn@users.noreply.github.com>
2024-07-11 18:02:14 +0200

    11-07-2024 added

---
+++ 11-07-2024/src/App.jsx
@@ -0,0 +1,21 @@
+//import styles from "./App.module.css";
+import Button from "./components/button/Button";
+import Counter from "./components/counter/Counter";
+import IconCart from "./components/icons/IconCart";
+import ProductLightBox from "./components/product-lightbox/ProductLightBox";
+
+function App() {
+  return (
+    <>
+      <h1>hello, world</h1>
+      <Button>
+        <IconCart fill="#000" />
+      </Button>
+      <Counter />
+
+      <ProductLightBox />
+    </>
+  );
+}
+
+export default App;
