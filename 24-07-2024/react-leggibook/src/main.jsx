7f755915bb047771cf78f60ae5cb97035f97e14d
Jiyuun <167774681+Jiyuunn@users.noreply.github.com>
2024-07-24 18:19:39 +0200

    24-07-2024 added

---
+++ 24-07-2024/react-leggibook/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import DefaultLayout from "./layouts/DefaultLayout.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BookDetailPage from "./pages/BookDetailPage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "books/:id",
+        element: <BookDetailPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
